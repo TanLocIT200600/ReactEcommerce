70785a501ce0c7941260d087a51f0b9631066355
TanLocIT200600 <77155886+TanLocIT200600@users.noreply.github.com>
2021-11-27 21:54:31 +0700

    1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Helmet from '../components/Helmet'
+import HeroSlider from '../components/HeroSlider'
+
+import heroSliderData from "../assets/fake-data/hero-slider"
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Helmet title="Trang chủ">
+      {/*start slider */}
+      <HeroSlider
+        data={heroSliderData}
+      />
+      {/* end slider */}
+    </Helmet>
   )
 }
 
