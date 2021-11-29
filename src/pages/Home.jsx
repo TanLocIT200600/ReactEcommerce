671e39cbbf798d3119d81064297a7c5d367c2056
TanLocIT200600 <77155886+TanLocIT200600@users.noreply.github.com>
2021-11-29 15:50:51 +0700

    40%

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Helmet from '../components/Helmet'
 import HeroSlider from '../components/HeroSlider'
+import Section, { SectionTitle, SectionBody } from '../components/Section'
+import PolicyCard from '../components/PolicyCard';
+import Grid from '../components/Grid'
 
 import heroSliderData from "../assets/fake-data/hero-slider"
+import policy from '../assets/fake-data/policy'
+import ProductCard from '../components/ProductCard';
+
+import productData from '../assets/fake-data/product'
+
+import banner from "../assets/images/banner.png"
 
 const Home = () => {
   return (
@@ -15,6 +25,106 @@ const Home = () => {
         timeOut={5000}
       />
       {/* end slider */}
+      {/* Section */}
+      <Section>
+        <SectionBody>
+          <Grid
+            col={4}
+            mdCol={2}
+            smCol={1}
+            gap={20}
+          >
+            {
+              policy.map((item, index) => (
+                <Link key={index} to="/policy">
+                  <PolicyCard
+                    name={item.name}
+                    description={item.description}
+                    icon={item.icon}
+                  />
+                </Link>
+              ))
+            }
+          </Grid>
+        </SectionBody>
+      </Section>
+      {/* Selling */}
+      <Section>
+        <SectionTitle>
+          top sản phẩm bán chạy trong tuần
+        </SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {
+              productData.getProducts(4).map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                />
+              ))
+            }
+          </Grid>
+        </SectionBody>
+      </Section>
+
+      {/* new arrival section */}
+      <Section>
+        <SectionTitle>
+          sản phẩm mới
+        </SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {
+              productData.getProducts(8).map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                />
+              ))
+            }
+          </Grid>
+        </SectionBody>
+      </Section>
+
+      {/* banner */}
+      <Section>
+        <SectionBody>
+          <Link to="/catalog">
+            <img src={banner} alt="" />
+          </Link>
+        </SectionBody>
+      </Section>
+
+      {/* popular product section */}
+      <Section>
+        <SectionTitle>
+          phổ biến
+        </SectionTitle>
+        <SectionBody>
+          <Grid col={4} mdCol={2} smCol={1} gap={20}>
+            {
+              productData.getProducts(12).map((item, index) => (
+                <ProductCard
+                  key={index}
+                  img01={item.image01}
+                  img02={item.image02}
+                  name={item.title}
+                  price={Number(item.price)}
+                  slug={item.slug}
+                />
+              ))
+            }
+          </Grid>
+        </SectionBody>
+      </Section>
     </Helmet>
   )
 }
